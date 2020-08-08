b37f4f0838bfa40b20f1c0ca207bf62cf1dbc72f
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-08 01:32:39 -0500

    Update server controller
    * Fix listen to allow custom value
    
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/vendor/ikoabo/controllers/server.controller.ts
+++ src/vendor/ikoabo/controllers/server.controller.ts
@@ -7,20 +7,20 @@
  * It can't be copied and/or distributed without the express
  * permission of the author.
  */
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import methodOverride from 'method-override'
-import cors from 'cors';
-import Helmet from 'helmet';
-import onFinished from 'on-finished';
-import moment from 'moment';
-import mongoose from 'mongoose';
-import { Request, Response, NextFunction } from 'express';
-import { Server, createServer } from 'http';
-import { Logger } from './logger.controller';
-import { ISettings } from '../models/settings.model';
-import { HTTP_STATUS } from '../middlewares/response.middleware';
+import express from "express";
+import bodyParser from "body-parser";
+import logger from "morgan";
+import methodOverride from "method-override";
+import cors from "cors";
+import Helmet from "helmet";
+import onFinished from "on-finished";
+import moment from "moment";
+import mongoose from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import { Server, createServer } from "http";
+import { Logger } from "./logger.controller";
+import { ISettings } from "../models/settings.model";
+import { HTTP_STATUS } from "../middlewares/response.middleware";
 
 /**
  * Standar Express Http Server handler
@@ -37,20 +37,20 @@ export class HttpServer {
    * Private constructor to allow singleton instance
    */
   private constructor() {
-    this._logger = new Logger('HttpServer');
+    this._logger = new Logger("HttpServer");
   }
 
   /**
    * Initialize the singleton server instance
-   * 
+   *
    * @param settings  Service predefined settings
    */
   public static setup(settings: ISettings): HttpServer {
     if (HttpServer._instance) {
-      throw new Error('HttpServer its initialized');
+      throw new Error("HttpServer its initialized");
     }
 
-    this._settings = settings
+    this._settings = settings;
     HttpServer._instance = new HttpServer();
     return HttpServer._instance;
   }
@@ -60,7 +60,7 @@ export class HttpServer {
    */
   public static get shared(): HttpServer {
     if (!HttpServer._instance) {
-      throw new Error('HttpServer not initialized');
+      throw new Error("HttpServer not initialized");
     }
     return HttpServer._instance;
   }
@@ -78,22 +78,28 @@ export class HttpServer {
   public initMongo(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       /* Connect to the MongoDB server */
-      mongoose.set('useFindAndModify', false);
-      mongoose.set('useCreateIndex', !HttpServer._settings.MONGODB.NOT_USE_CREATE_INDEX);
-      mongoose.connect(HttpServer._settings.MONGODB.URI, {
-        useNewUrlParser: !HttpServer._settings.MONGODB.NOT_USE_NEW_URL_PARSER,
-        useCreateIndex: !HttpServer._settings.MONGODB.NOT_USE_CREATE_INDEX,
-        autoIndex: !HttpServer._settings.MONGODB.NOT_AUTO_INDEX,
-        poolSize: HttpServer._settings.MONGODB.POOL_SIZE || 10,
-        useUnifiedTopology: !HttpServer._settings.MONGODB.NOT_USE_UNIFIED_TOPOLOGY,
-      }).then(() => {
-        this._logger.info('Connected to MongoDB', { worker: this.worker });
-        resolve();
-      });
+      mongoose.set("useFindAndModify", false);
+      mongoose.set(
+        "useCreateIndex",
+        !HttpServer._settings.MONGODB.NOT_USE_CREATE_INDEX
+      );
+      mongoose
+        .connect(HttpServer._settings.MONGODB.URI, {
+          useNewUrlParser: !HttpServer._settings.MONGODB.NOT_USE_NEW_URL_PARSER,
+          useCreateIndex: !HttpServer._settings.MONGODB.NOT_USE_CREATE_INDEX,
+          autoIndex: !HttpServer._settings.MONGODB.NOT_AUTO_INDEX,
+          poolSize: HttpServer._settings.MONGODB.POOL_SIZE || 10,
+          useUnifiedTopology: !HttpServer._settings.MONGODB
+            .NOT_USE_UNIFIED_TOPOLOGY,
+        })
+        .then(() => {
+          this._logger.info("Connected to MongoDB", { worker: this.worker });
+          resolve();
+        });
 
       /* Listen for MongoDB error connection */
-      mongoose.connection.on('error', (err: any) => {
-        this._logger.error('MongoDB cannot establish the connection', err)
+      mongoose.connection.on("error", (err: any) => {
+        this._logger.error("MongoDB cannot establish the connection", err);
         reject(err);
       });
     });
@@ -119,9 +125,9 @@ export class HttpServer {
 
       /* Check to enable method override */
       if (!HttpServer._settings.SERVICE.NOT_METHOD_OVERRIDE) {
-        this._app.use(methodOverride('X-HTTP-Method')) // Microsoft
-        this._app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
-        this._app.use(methodOverride('X-Method-Override')) // IBM
+        this._app.use(methodOverride("X-HTTP-Method")); // Microsoft
+        this._app.use(methodOverride("X-HTTP-Method-Override")); // Google/GData
+        this._app.use(methodOverride("X-Method-Override")); // IBM
       }
 
       /* Check to enable CORS */
@@ -133,57 +139,72 @@ export class HttpServer {
        * Security mechanism
        */
       this._app.use(Helmet.xssFilter());
-      this._app.disable('x-powered-by');
+      this._app.disable("x-powered-by");
       this._app.use(Helmet.hidePoweredBy());
-      this._app.use(Helmet.frameguard({ action: 'deny' }));
+      this._app.use(Helmet.frameguard({ action: "deny" }));
       this._app.use(Helmet.noSniff());
       this._app.use(Helmet.referrerPolicy());
       this._app.use(Helmet.ieNoOpen());
-      this._app.use(Helmet.hsts({
-        maxAge: 5184000
-      }));
+      this._app.use(
+        Helmet.hsts({
+          maxAge: 5184000,
+        })
+      );
 
       /* Set trust proxy */
-      this._app.set('trust proxy', !HttpServer._settings.SERVICE.NOT_TRUST_PROXY);
+      this._app.set(
+        "trust proxy",
+        !HttpServer._settings.SERVICE.NOT_TRUST_PROXY
+      );
 
       // Express configuration
-      this._app.set("interface", HttpServer._settings.SERVICE.INTERFACE || '127.0.0.1');
+      this._app.set(
+        "interface",
+        HttpServer._settings.SERVICE.INTERFACE || "127.0.0.1"
+      );
       this._app.set("port", HttpServer._settings.SERVICE.PORT);
       this._app.set("env", HttpServer._settings.SERVICE.ENV);
 
       /* Increment debug output on offline development platforms */
-      if (HttpServer._settings.SERVICE.ENV !== 'production') {
-        this._app.use(logger('dev'));
-        this._app.all('/*', (req: Request, res: Response, next: NextFunction) => {
-          onFinished(res, (err: any, resp: any) => {
-            let requestTrace: any = {
-              stamp: moment.utc().toDate().getTime(),
-              err: err,
-              req: {
-                method: req.method,
-                url: req.originalUrl,
-                body: req.body,
-                headers: req.headers
-              },
-              res: {
-                status: resp.statusCode,
-                message: resp.statusMessage
-              },
-              worker: this.worker
-            };
-            this._logger.debug(' Request trace', requestTrace);
-          });
-          next();
-        });
+      if (HttpServer._settings.SERVICE.ENV !== "production") {
+        this._app.use(logger("dev"));
+        this._app.all(
+          "/*",
+          (req: Request, res: Response, next: NextFunction) => {
+            onFinished(res, (err: any, resp: any) => {
+              let requestTrace: any = {
+                stamp: moment.utc().toDate().getTime(),
+                err: err,
+                req: {
+                  method: req.method,
+                  url: req.originalUrl,
+                  body: req.body,
+                  headers: req.headers,
+                },
+                res: {
+                  status: resp.statusCode,
+                  message: resp.statusMessage,
+                },
+                worker: this.worker,
+              };
+              this._logger.debug(" Request trace", requestTrace);
+            });
+            next();
+          }
+        );
       } else {
-        this._app.use(logger('tiny'));
+        this._app.use(logger("tiny"));
       }
 
       /* Check to retrieve the real IP address of the request */
       if (!HttpServer._settings.SERVICE.NOT_REAL_IP) {
         this._app.use((req: any, res: Response, next: NextFunction) => {
           /* Look for request IP address */
-          res.locals['ipAddr'] = req.headers['x-caller-ip'] || req.headers['x-forwarded-for'] || req.ips[0] || req.connection.remoteAddress;
+          res.locals["ipAddr"] =
+            req.headers["x-caller-ip"] ||
+            req.headers["x-forwarded-for"] ||
+            req.ips[0] ||
+            req.connection.remoteAddress;
           next();
         });
       }
@@ -203,7 +224,7 @@ export class HttpServer {
 
   /**
    * Add new middleware to the Express application
-   * 
+   *
    * @param middleware  Express middleware to be registered
    * @param route  Router or array of routers to handle the given middleware
    */
@@ -218,21 +239,25 @@ export class HttpServer {
   /**
    * Start listening on the HTTP server
    */
-  public listen(): Promise<any> {
-    return new Promise<any>((resolve) => {
-      this._http.listen(this._app.get('port'), this._app.get('interface'), () => {
-        let meta: any = {
-          interface: this._app.get('interface'),
-          port: this._app.get('port'),
-          env: this._app.get('env'),
-          version: `${HttpServer._settings.VERSION.MAIN}.${HttpServer._settings.VERSION.MINOR}.${HttpServer._settings.VERSION.REVISION}`,
-          pid: process.pid,
-          worker: this.worker
-        };
+  public listen(port?: number): Promise<Server> {
+    return new Promise<Server>((resolve) => {
+      const server: Server = this._http.listen(
+        port === null ? this._app.get("port") : port,
+        this._app.get("interface"),
+        () => {
+          let meta: any = {
+            interface: this._app.get("interface"),
+            port: this._app.get("port"),
+            env: this._app.get("env"),
+            version: `${HttpServer._settings.VERSION.MAIN}.${HttpServer._settings.VERSION.MINOR}.${HttpServer._settings.VERSION.REVISION}`,
+            pid: process.pid,
+            worker: this.worker,
+          };
 
-        this._logger.info('Service instance is running', meta);
-        resolve();
-      });
+          this._logger.info("Service instance is running", meta);
+          resolve(server);
+        }
+      );
     });
   }
 
@@ -245,7 +270,7 @@ export class HttpServer {
     /* Check if default routes must be set */
     if (routes) {
       const keys = Object.keys(routes);
-      keys.forEach(key => {
+      keys.forEach((key) => {
         /* Check if the route is an array of routers or not */
         if (Array.isArray(routes[key])) {
           routes[key].forEach((value: any) => {
@@ -259,7 +284,10 @@ export class HttpServer {
 
     /* Other routes give 404 error */
     this._app.use(function (_req, res, _next) {
-      res.status(HTTP_STATUS.HTTP_NOT_FOUND).json({ error: HTTP_STATUS.HTTP_NOT_FOUND }).end();
+      res
+        .status(HTTP_STATUS.HTTP_NOT_FOUND)
+        .json({ error: HTTP_STATUS.HTTP_NOT_FOUND })
+        .end();
     });
   }
 }
